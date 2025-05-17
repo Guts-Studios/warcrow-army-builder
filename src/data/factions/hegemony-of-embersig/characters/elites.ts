77ac8f0591e1ed431038de725592324a89474993
jayroi <78560919+jayroi@users.noreply.github.com>
2025-05-17 14:35:11 -0700

    Update elites.ts

--- src/data/factions/hegemony-of-embersig/characters/elites.ts
+++ src/data/factions/hegemony-of-embersig/characters/elites.ts
@@ -34,6 +34,6 @@ export const hegemonyCharactersElites: Unit[] = [
     ],
     specialRules: ["Frightened", "Aim", "Repeat a Die"],
     highCommand: false,
-    imageUrl: "/art/card/nadezhda_lazard_champion_of_embersig_card.jpg" // Fixed path to card image
+    imageUrl: "/art/card/lady_telia_card.jpg"
   }
 ];
