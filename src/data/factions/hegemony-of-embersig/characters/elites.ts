82ffb57646c7c08d4702a752a9a2a23a7702fb17
jayroi <78560919+jayroi@users.noreply.github.com>
2025-05-17 14:59:08 -0700

    Update elites.ts

--- src/data/factions/hegemony-of-embersig/characters/elites.ts
+++ src/data/factions/hegemony-of-embersig/characters/elites.ts
@@ -20,7 +20,7 @@ export const hegemonyCharactersElites: Unit[] = [
   },
   {
     id: "lady-telia",
-    name: "Lady Telia",
+    name: "Lady Télia",
     faction: "hegemony-of-embersig",
     pointsCost: 25,
     availability: 1,
