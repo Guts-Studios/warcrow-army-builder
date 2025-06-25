7158468c42f02e4b820888ed4cfed088293063fe
James Caldwell <jcaldwell2@radford.edu>
2025-06-25 11:17:12 -0400

    Update duplicate units to consistent data

--- src/data/factions/northern-tribes/characters/tattooist.ts
+++ src/data/factions/northern-tribes/characters/tattooist.ts
@@ -2,20 +2,22 @@
 import { Unit } from "@/types/army";
 
 export const tattooist: Unit = {
-  id: "tattooist",
-  name: "Tattooist",
-  pointsCost: 15,
-  faction: "northern-tribes",
-  faction_id: "northern-tribes",
-  keywords: [
-    { name: "Character", description: "" },
-    { name: "Varank", description: "" },
-    { name: "Join (Infantry Varank)", description: "" },
-    { name: "Elite", description: "" }
-  ],
-  highCommand: false,
-  availability: 1,
-  command: 0,
-  specialRules: [],
-  imageUrl: "/art/card/tattooist_card.jpg"
-};
+    id: "tattooist",
+    name: "Tattooist",
+    name_es: "Tatuadora",
+    pointsCost: 15,
+    faction: "northern-tribes",
+    faction_id: "northern-tribes",
+    keywords: [
+      { name: "Character", description: "" },
+      { name: "Varank", description: "" },
+      { name: "Join (Infantry Varank)", description: "" },
+      { name: "Elite", description: "" }
+    ],
+    highCommand: false,
+    availability: 1,
+    command: 0,
+    specialRules: [],
+    tournamentLegal: true,
+    imageUrl: "/art/card/tattooist_card.jpg"
+  };
